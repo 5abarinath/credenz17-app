e6f0d02d1e054792633760787e49dcf43be597eb
5abarinath <sabarinath1996@gmail.com>
2017-06-24 14:15:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/pisb/credenz17/splashScreenActivity.java
@@ -0,0 +1,13 @@
+package com.pisb.credenz17;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
